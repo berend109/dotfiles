11b77e3f1146bc10cc0389cfe72cbd5258d71700
Berend Folkers <16764030+berend109@users.noreply.github.com>
2023-06-07 02:06:27 +0200

    next version

--- nvim/lua/berend/packer.lua
+++ nvim/lua/berend/packer.lua
@@ -26,26 +26,47 @@ return require('packer').startup(function(use)
 	use ('mbbill/undotree')
 	use ('tpope/vim-fugitive')
 
+	-- use {
+	-- 	'VonHeikemen/lsp-zero.nvim',
+	-- 	branch = 'v1.x',
+	-- 	requires = {
+	-- 		-- LSP Support
+	-- 		{'neovim/nvim-lspconfig'},
+	-- 		{'williamboman/mason.nvim'},
+	-- 		{'williamboman/mason-lspconfig.nvim'},
+
+	-- 		-- Autocompletion
+	-- 		{'hrsh7th/nvim-cmp'},
+	-- 		{'hrsh7th/cmp-buffer'},
+	-- 		{'hrsh7th/cmp-path'},
+	-- 		{'saadparwaiz1/cmp_luasnip'},
+	-- 		{'hrsh7th/cmp-nvim-lsp'},
+	-- 		{'hrsh7th/cmp-nvim-lua'},
+
+	-- 		-- Snippets
+	-- 		{'L3MON4D3/LuaSnip'},
+	-- 		{'rafamadriz/friendly-snippets'},
+	-- 	}
+	-- }
+
 	use {
 		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
+		branch = 'v2.x',
 		requires = {
 			-- LSP Support
-			{'neovim/nvim-lspconfig'},
-			{'williamboman/mason.nvim'},
-			{'williamboman/mason-lspconfig.nvim'},
+			{'neovim/nvim-lspconfig'},             -- Required
+			{                                      -- Optional
+			'williamboman/mason.nvim',
+			run = function()
+				pcall(vim.cmd, 'MasonUpdate')
+			end,
+			},
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
 
 			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},
-			{'hrsh7th/cmp-buffer'},
-			{'hrsh7th/cmp-path'},
-			{'saadparwaiz1/cmp_luasnip'},
-			{'hrsh7th/cmp-nvim-lsp'},
-			{'hrsh7th/cmp-nvim-lua'},
-
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},
-			{'rafamadriz/friendly-snippets'},
+			{'hrsh7th/nvim-cmp'},     -- Required
+			{'hrsh7th/cmp-nvim-lsp'}, -- Required
+			{'L3MON4D3/LuaSnip'},     -- Required
 		}
 	}
 end)
