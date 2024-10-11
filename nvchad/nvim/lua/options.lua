3a8a1cdb66b6fc8aef49b858a2702e6f56690f01
Berend <berend@MiniVanBerend.localdomain>
2024-10-11 10:45:35 +0200

    big chad move

---
+++ nvchad/nvim/lua/options.lua
@@ -0,0 +1,10 @@
+require "nvchad.options"
+
+-- add yours here!
+vim.wo.relativenumber = true
+vim.g.nvchad_theme = "gruvbox"
+vim.wo.list = true
+vim.wo.listchars = 'tab:┌┬┐,trail:·'
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
